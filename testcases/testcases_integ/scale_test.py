bba34fc92510a7989f5d9199b833c87304937920
root <root@f4-controller.noiro.lab>
2015-07-10 16:42:18 -0700

    1. fix the issue that prevents PTGs & L2 policies from being deleted 2. also add the deletion of L3 policy at the end 3. also imrpove the naming of a variable

--- testcases/testcases_integ/scale_test.py
+++ testcases/testcases_integ/scale_test.py
@@ -10,27 +10,31 @@ from time import sleep
 from libs.raise_exceptions import *
 from libs.gbp_crud_libs import GBPCrud
 
-x = GBPCrud('172.28.184.45')
-l3p = x.create_gbp_l3policy('scale_l3p',ip_pool='1.2.3.0/16',subnet_prefix_length=30)
-l3p_id = x.verify_gbp_l3policy('scale_l3p')
+gdb_crud = GBPCrud('172.28.184.45')
+
+l3p = gdb_crud.create_gbp_l3policy('scale_l3p',ip_pool='1.2.3.0/16',subnet_prefix_length=30)
+l3p_id = gdb_crud.verify_gbp_l3policy('scale_l3p')
 print 'L3Policy == ', l3p_id
+
 for i in range(1,101):
     l2p_name = 'scale_l2p_%s' %(i)
-    x.create_gbp_l2policy(l2p_name,l3_policy_id=l3p_id)
+    gdb_crud.create_gbp_l2policy(l2p_name,l3_policy_id=l3p_id)
     sleep(1)
-    l2p_id = x.verify_gbp_l2policy(l2p_name)
+    l2p_id = gdb_crud.verify_gbp_l2policy(l2p_name)
     print 'L2P ID == ', l2p_id
     ptg_name = 'scale_ptg_%s' %(i)
-    x.create_gbp_policy_target_group(ptg_name,l2_policy_id=l2p_id)
+    gdb_crud.create_gbp_policy_target_group(ptg_name,l2_policy_id=l2p_id)
 
-ptg_list = x.get_gbp_policy_target_group_list(getlist=True)
-l2p_list_ids = x.get_gbp_l2policy_list()
+ptg_list = gdb_crud.get_gbp_policy_target_group_list(getlist=True)
+l2p_list_ids = gdb_crud.get_gbp_l2policy_list(getlist=True)
 
 print 'PTG LIST /NAME == \n', ptg_list
 print '\n L2POLICY LIST IDs ==\n', l2p_list_ids
 
 for val in ptg_list.itervalues():
-    x.delete_gbp_policy_target_group(val)
+    gdb_crud.delete_gbp_policy_target_group(val, 'uuid')
 
-for l2p in l2p_list_ids:
-    x.delete_gbp_l2policy(l2p)
+for l2p in l2p_list_ids.itervalues():
+    gdb_crud.delete_gbp_l2policy(l2p, 'uuid')
+
+gdb_crud.delete_gbp_l3policy(l3p_id, 'uuid')
