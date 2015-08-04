53cbaeb2daa33ce0d2e2640b8ca6b70a2d3424d3
Kent Wu <wu@noironetworks.com>
2015-08-04 16:12:01 -0700

    use the specific tenant name to launch those VMs belonging to this tenant

--- testcases/scale/launch_vm.py
+++ testcases/scale/launch_vm.py
@@ -1,25 +1,28 @@
 #!/usr/bin/env python
 import sys
-import sys
 import logging
 import os
 import datetime
 import string
 import pdb
-from time import sleep
 from libs.raise_exceptions import *
 from libs.gbp_crud_libs import GBPCrud
 from libs.gbp_nova_libs import Gbp_Nova
 
-gbp_crud = GBPCrud(sys.argv[1])
-pt_dict = gbp_crud.get_gbp_policy_target_list()
-print '\nPT Dict == \n', pt_dict
+for j in range(1, 6):                                                                                      
 
-gbp_nova = Gbp_Nova(sys.argv[1])
+    tenantName = 'scale_tenant_%s' % (j)
+    gbp_crud = GBPCrud(sys.argv[1], tenant=tenantName)
 
-i = 1
-for pt in pt_dict.itervalues():
-    vm_name = 'scale_vm_%s' %(i)   
-    gbp_nova.vm_create_api(vm_name, 'cirros', pt, 'm1.tiny', 'nova')
-    #gbp_nova.vm_delete(vm_name, 'api')
-    i = i + 1
+    pt_dict = gbp_crud.get_gbp_policy_target_list()
+    print '\nPT Dict == \n', pt_dict
+
+    gbp_nova = Gbp_Nova(sys.argv[1], os_tenant=tenantName)
+    
+    i = 1
+    for pt in pt_dict.itervalues():
+        vm_name = '%s_scale_vm_%s' % (j, i)   
+        print vm_name
+        gbp_nova.vm_create_api(vm_name, 'cirros', pt, 'm1.tiny', 'nova')
+        #gbp_nova.vm_delete(vm_name, 'api')
+        i = i + 1
