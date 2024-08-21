248309a5cc780daa0a4548e78badf1cf684b44a2
Iftikhar Rathore <irathore@cisco.com>
2024-08-21 12:47:06 -0700

    Changed SECLEVEL for older versions of ACI

--- libs/gbp_aci_libs.py
+++ libs/gbp_aci_libs.py
@@ -17,6 +17,7 @@
 import re
 import json
 import requests
+requests.packages.urllib3.util.ssl_.DEFAULT_CIPHERS += '@SECLEVEL=1' 
 import sys
 from fabric.api import cd,run,env, hide, get, settings, local, lcd
 from libs.raise_exceptions import *
